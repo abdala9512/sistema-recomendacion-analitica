fd73e6e3561b81e6093f4d30d3102ac4353960b3
abdala9512 <miguel.arquez12@gmail.com>
2021-08-28 20:56:49 -0500

    Comentarios adicionales sobre el proceso:

--- src/instacart_data_generations.R
+++ src/instacart_data_generations.R
@@ -2,14 +2,16 @@ library(readr)
 library(dplyr)
 
 
-set.seed(1008)
 
 orders               <- read_csv('./data/orders.csv') %>%  select(-eval_set)
 products             <- read_csv('./data/products.csv')
 aisles               <- read_csv('./data/aisles.csv')
 departments          <- read_csv('./data/departments.csv')
 
+# Semilla para reproduciblidad
+set.seed(1008)
 
+# Muestra de 10.00 clientes aleatorios
 users_sample <- sample.int(length(unique(orders$user_id)), 10000)
 
 orders %<>%
@@ -17,6 +19,7 @@ orders %<>%
 
 orders_id <- unique(orders$order_id)
 
+# Aca estoy uniendo los datos de ambas tablas, son los mismos pero separados con proposito de la competencia de kaggle
 order_products <-
   rbind(
     read_csv('./data/order_products__train.csv'),
@@ -24,13 +27,15 @@ order_products <-
   ) %>% filter(order_id %in% orders_id)
 
 
+
+# Unifico los datos de los productos en un solo catalogo
 product_catalog <-  products %>%
   left_join(aisles, by = "aisle_id") %>%
   left_join(departments, by = "department_id") %>%
   select(-aisle_id, -department_id)
 
 
-
+# Dejo 3 tablas finales con la muestra de 10.00 clientes
 save(order_products,
      product_catalog,
      orders,
