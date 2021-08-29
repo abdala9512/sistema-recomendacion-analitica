acf6381595d8efab00840cc3448462ea4e6add36
abdala9512 <miguel.arquez12@gmail.com>
2021-08-28 20:52:36 -0500

    Mod: muestra de datos para preparar analisis de reglas

--- src/instacart_data_generations.R
+++ src/instacart_data_generations.R
@@ -2,6 +2,7 @@ library(readr)
 library(dplyr)
 
 
+set.seed(1008)
 
 orders               <- read_csv('./data/orders.csv') %>%  select(-eval_set)
 products             <- read_csv('./data/products.csv')
@@ -9,12 +10,18 @@ aisles               <- read_csv('./data/aisles.csv')
 departments          <- read_csv('./data/departments.csv')
 
 
+users_sample <- sample.int(length(unique(orders$user_id)), 10000)
+
+orders %<>%
+  filter(user_id %in% users_sample)
+
+orders_id <- unique(orders$order_id)
+
 order_products <-
   rbind(
     read_csv('./data/order_products__train.csv'),
     read_csv('./data/order_products__prior.csv')
-  )
-
+  ) %>% filter(order_id %in% orders_id)
 
 
 product_catalog <-  products %>%
@@ -27,7 +34,7 @@ product_catalog <-  products %>%
 save(order_products,
      product_catalog,
      orders,
-     file =  "./data/instcart_data.RData")
+     file =  "./data/instcart_data_sample.RData")
 
 
 rm(list = ls()); gc()
